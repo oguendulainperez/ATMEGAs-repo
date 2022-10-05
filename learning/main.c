3dd58038d1a709149d83c58707aeb869a89a8e6e
oguendulainperez <113444328+oguendulainperez@users.noreply.github.com>
2022-10-04 20:22:43 -0500

    Reverted Project

--- learning/main.c
+++ learning/main.c
@@ -2,13 +2,28 @@
 *This is a learning for me to use git tools
 */
 
-#int <stdio.h>
+#include <stdio.h>
 
-int main ( ){
+//#include "/home/razielgdn/ATMEGAs-repo/learning/arithmetics.h"
+//#include "arithmetics.c"
+#include "arithmetics.h"
+int main(){
+	//int numA, int numB;
+	printf("Hello World!!\n");
+	printf("I need a lot of money\n");
+	printf("intoduce primer numero\n");	
+	scanf("%d", &variableA);
 
-	printf ("Hello World!!");
-	printf ("I need a lot of money");
-	return 0
+	printf("intoduce segundo numero\n");	
+	scanf("%d", &variableB);
+	
+	printf ("La suma de los 2 numeros es: %d + %d = %d\n",variableA, variableB, add( variableA, variableB));
+	printf("La division de los 2 numeros es: %d + %d = %f\n",variableA, variableB, divide( variableA, variableB));
+	
+	/*
+	Agregando comentarios al proyecto
+		*/
+	return 0;
 }
 
 
